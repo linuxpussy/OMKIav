02f80eacac089e648292c74130ccfdd7ecdbc9de
linuxpussy <63728887+linuxpussy@users.noreply.github.com>
2020-08-01 21:12:28 +0200

    Create install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+os.system("cd /home/$USER && function omkiavscan() {
+echo 'building engine' && $pppppppp =  ls /home/$USER if $pppppppp == "bladraxoppppl.exe"; 
+then; 
+echo "iruska.pyslide found"
+else
+      echo "secure"
+")
+touch severe.engine
+
